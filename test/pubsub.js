96ab8c0114c40028a8e09a301886389f7b5f01a8
Juca <clovis.junior@baritecnologia.com.br>
2024-03-21 22:14:20 -0300

    chore: BTC price

--- test/pubsub.js
+++ test/pubsub.js
@@ -2,6 +2,7 @@ const {
   IoTDataPlaneClient,
   PublishCommand,
 } = require("@aws-sdk/client-iot-data-plane");
+const { resolve } = require("styled-jsx/css");
 
 const {
   AWS_REGION,
@@ -41,4 +42,33 @@ async function publish(topic, data) {
   }
 }
 
-publish(AWS_IOT_TOPIC, "Hi from lambda");
+async function main(symbol = "BTC") {
+  publish(AWS_IOT_TOPIC, "CLEAR");
+
+  while (true) {
+    const time = new Date().toTimeString().substring(0, 8);
+
+    try {
+      const res = await fetch(
+        `https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=1m&limit=${1}`
+      );
+      const json = await res.json();
+      const [, , , , closed] = json[0];
+
+      const price = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(Number(closed));
+
+      const display = `${symbol} ${price} ${time}`;
+      publish(AWS_IOT_TOPIC, display);
+
+      await new Promise((resolve) => setTimeout(resolve, 30000));
+    } catch (err) {
+      console.error(err);
+      publish(AWS_IOT_TOPIC, `ERR ${time}`);
+    }
+  }
+}
+
+main().then(console.log).catch(console.error);
